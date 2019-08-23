f690083288da82a902efca035e1478bc2965003b
刘向明 <719327306@qq.com>
2019-08-23 09:36:40 +0800

    for animate1

--- js/js/util.js
+++ js/js/util.js
@@ -1,3 +1,4 @@
+//匀速动画
 function animate(obj,attr,target){
 	clearInterval(obj.timer);
 	var iSpeed = 0;
@@ -26,6 +27,29 @@ function animate(obj,attr,target){
 			}
 			// console.log(current)
 		}
+	},30)
+}
+//减速动画
+function animate1(obj,attr,target){
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var current  = parseFloat(getComputedStyle(obj,false)[attr]);
+		if(attr == 'opacity'){
+			current = Math.round(current *100);
+		}
+		var iSpeed = 0;
+		iSpeed = (target - current)/10;
+		iSpeed = iSpeed >0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+		if(!iSpeed){
+			clearInterval(obj.timer);
+		}else{
+			if(attr == 'opacity'){
+				obj.style[attr] = (current + iSpeed)/100;
+			}else{
+				obj.style[attr] = current + iSpeed +'px';
+			}
+		}
+		console.log(current,iSpeed)
 	},30)
 }
 function getScrollTop(){
