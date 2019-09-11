47a78b03686b135a4512f5a23aa24ccac41cc983
刘向明 <719327306@qq.com>
2019-09-11 16:36:19 +0800

    ajax

---
+++ 04-js-进阶/041-server4.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var fs  = require('fs');
+
+var server = http.createServer(function(req, res){
+	// console.log(req.method);
+	var urlStr = req.url;
+	if(req.method == 'POST'){
+		var body = '';
+		req.on('data',function(chunk){
+			body += chunk;
+		});
+		req.on('end',function(){
+			console.log('get post data::',body);
+			//根据数据做处理....
+			res.end(body);
+		})
+	}else if(req.method == 'GET'){
+		if(urlStr == '/favicon.ico'){
+			res.end('favicon.ico')
+		}
+		var filePath  = './'+urlStr;
+		fs.readFile(filePath,function(err,data){
+			if(!err){
+				res.end(data);
+			}else{
+				res.statusCode = 404;
+				res.end('not found...')
+			}
+		})
+	}
+	
+});
+
+server.listen(3000, '127.0.0.1', function(){
+  console.log("Server running at http://127.0.0.1:3000");
+});
