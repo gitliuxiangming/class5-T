3645966837f359bf9564004abf124cbf4bb47c9a
刘向明 <719327306@qq.com>
2019-09-26 18:43:52 +0800

    search

--- 03-project/ktao/js/search.js
+++ 03-project/ktao/js/search.js
@@ -2,12 +2,18 @@
 	function Search($elem,options){
 		//1.罗列属性
 		this.$elem = $elem;
+		this.options = options;
 		this.$searchBtn = $elem.find('.search-btn')
 		this.$searchInput = $elem.find('.search-input')
+		this.$searchForm = $elem.find('.search-form')
+		this.$searchLayer = $elem.find('.search-layer')
 	
 		
 		//2.初始化
 		this.init();
+		if(this.options.autocomplete){
+			this.autocomplete();
+		}
 	}
 	Search.prototype = {
 		constructor:Search,
@@ -19,14 +25,54 @@
 			if(val == ''){
 				return false;
 			}
+
+			this.$searchForm.trigger('submit');
 		},
 		getValue:function(){
 			return this.$searchInput.val();
+		},
+		autocomplete:function(){
+			//1.将显示隐藏插件初始化
+			this.$searchLayer.showHide(this.options);
+			//2.监听输入框的oninput事件
+			this.$searchInput.on('input',$.proxy(this.getData,this))
+			
+		},
+		getData:function(){
+			$.ajax({
+				url:this.options.url+this.getValue(),
+				dataType:'jsonp',
+				jsonp:'callback'
+			})
+			.done(function(data){
+				// console.log(data)
+				//1.将数据包装成html代码
+				var html = '';
+				for(var i=0;i<data.result.length;i++){
+					html += '<li class="search-item">'+data.result[i][0]+'</li>'
+				}
+				// console.log(html)
+
+				//2.将html代码放入到下拉层中
+				this.$searchLayer.html(html)
+
+				//3.将下拉层展示出来
+				this.$searchLayer.showHide('show')
+
+
+
+			}.bind(this))
+			.fail(function(err){
+				console.log(err)
+			})
 		}
 	}
-
+	//https://suggest.taobao.com/sug?code=utf-8&q=a&_ksTS=1569493529438_1927&callback=jsonp1928&k=1&area=c2c&bucketid=16
 	Search.DEFAULTS = {
-		
+		autocomplete:true,
+		url:'https://suggest.taobao.com/sug?q=',
+		js:true,
+		mode:'slideDownUp'
 	}
 
 
