90b72284c7e003478ff1bca1a147e74a4bec556b
刘向明 <719327306@qq.com>
2019-10-09 18:23:13 +0800

    carousel

--- 03-project/ktao/js/carousel.js
+++ 03-project/ktao/js/carousel.js
@@ -16,16 +16,50 @@
 		constructor:Carousel,
 		init:function(){
 			if(this.options.slide){//划入划出
-				
+				//将所有图片隐藏
+				this.$elem.addClass('slide');
+				//默认显示
+				this.$carouselItem.eq(this.now).css({left:0})
+				// this.$btns.eq(this.now).addClass('active');
+				//将移动插件初始化
+				this.$carouselItem.move(this.options);
+				//将左右按钮显示出来(监听事件)
+				/*
+				this.$elem
+				.hover(function(){
+					this.$control.show();
+				}.bind(this),function(){
+					this.$control.hide();
+				}.bind(this))
+				.on('click','.control-left',function(){
+					this._slide(this.getIndex(this.now - 1))
+				}.bind(this))
+				.on('click','.control-right',function(){
+					this._slide(this.getIndex(this.now + 1))
+				}.bind(this))
+				//处理自动轮播
+				if(this.options.interval){
+					this.autoplay();
+					this.$elem.hover($.proxy(this.paused,this),$.proxy(this.autoplay,this));
+				}
+				//处理底部按钮的点击事件
+				var _this = this;
+				this.$btns.on('click',function(){
+					//获取对应的下标
+					_this._slide(_this.$btns.index($(this)))
+				})
+				*/
+				this.tab = this._slide;
 			}else{//淡入淡出
 				//将所有图片隐藏
 				this.$elem.addClass('fade');
 				//默认显示
 				this.$carouselItem.eq(this.now).show();
-				this.$btns.eq(this.now).addClass('active');
+				// this.$btns.eq(this.now).addClass('active');
 				//将显示隐藏插件初始化
-				this.$carouselItem.showHide()
+				this.$carouselItem.showHide(this.options)
 				//将左右按钮显示出来(监听事件)
+				/*
 				this.$elem
 				.hover(function(){
 					this.$control.show();
@@ -49,7 +83,36 @@
 					//获取对应的下标
 					_this._fade(_this.$btns.index($(this)))
 				})
+				*/
+				this.tab = this._fade;
 			}
+			this.$btns.eq(this.now).addClass('active');
+			this.$elem
+			.hover(function(){
+				this.$control.show();
+			}.bind(this),function(){
+				this.$control.hide();
+			}.bind(this))
+			.on('click','.control-left',function(){
+				this.tab(this.getIndex(this.now - 1))
+			}.bind(this))
+			.on('click','.control-right',function(){
+				this.tab(this.getIndex(this.now + 1))
+			}.bind(this))
+			//处理自动轮播
+			if(this.options.interval){
+				this.autoplay();
+				this.$elem.hover($.proxy(this.paused,this),$.proxy(this.autoplay,this))
+			}
+			//处理底部按钮的点击事件
+			var _this = this;
+			this.$btns.on('click',function(){
+				//获取对应的下标
+				_this.tab(_this.$btns.index($(this)))
+			})
+		},
+		_slide:function(){
+			console.log('slide:::')
 		},
 		_fade(index){
 			//如果当前值和即将显示的值相等的话，不执行
